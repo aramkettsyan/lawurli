aeeaddf695bc4382967bb62d02e4aaf21ab5d248
aram <aram@st-dev.com>
2015-08-19 19:23:39 +0400

    user profile

--- views/users/profile.php
+++ views/users/profile.php
@@ -1,6 +1,8 @@
 
 
 <?php Yii::$app->view->params['user'] = $user; ?>
+<?php Yii::$app->view->params['sections'] = $sections; ?>
+<?php Yii::$app->view->params['user_forms'] = $user_forms; ?>
 <div class="container mainContainer">
     <div class="profileL">
         <div class="userImage">
@@ -10,7 +12,7 @@
                 <span class="profileImage" style="background-image: url('<?php echo \Yii::getAlias('@web') . '/images/users_images/' . $user->image; ?>')"></span>
             </div>
 
-        <!--<img src="<?php echo \Yii::getAlias('@web') . '/images/users_images/' . $user->image; ?>"  alt="User image" >-->
+                                                                        <!--<img src="<?php echo \Yii::getAlias('@web') . '/images/users_images/' . $user->image; ?>"  alt="User image" >-->
         <?php } ?>
         <p style="color:green;display: none" id="imageUploadSuccess">Image uploaded successfully!</p>
         <p style="color:red;display: none" id="imageUploadError"></p>
@@ -35,6 +37,9 @@
                 </li>
             </ul>
         </div>
+        <div class="alignCenter">
+            <a href="<?= \yii\helpers\Url::to(['users/edit']) ?>" class="btn defBtn">Edit profile</a>
+        </div>
     </div>
     <div class="profileR">
         <div class="profileTabs">
@@ -45,76 +50,98 @@
             </ul>
         </div>
         <div class="tabsContent">
-            <div class="cvTimeline">
-                <h4>Education</h4>
-                <div class="cvSub">
-                    <div class="cvSubLabel">
-                        <h5>University</h5>
-                    </div>
-                    <div class="cvSubCont">
-                        <ul>
-                            <li>
-                                <label class="cvSingleLabel">University Name</label>
-                                <p class="cvSingleDet">Blah blah University</p>
-                                <div class="labelValue">
-                                    <label>Some Label</label>
-                                    <span>Blah blha value</span>
+            <?php foreach ($this->params['sections'] as $sectionName => $section) { ?>
+                <div class="cvTimeline">
+                    <h4><?= $sectionName ?></h4>
+
+                    <?php foreach ($section as $subSectionName => $subSection) { ?>
+                        <div class="cvSub sub_section">
+                            <?php if ($subSectionName) { ?>
+                                <div class="cvSubLabel">
+                                    <h5><?= $subSectionName ?></h5>
                                 </div>
-                                <div class="labelValue">
-                                    <label>Another label</label>
-                                    <span>Blah blha value</span>
-                                </div>
-                            </li>
-                            <li>
-                                <p class="cvSingleTitle">At eos, delectus cum deleniti non.</p>
-                                <p class="cvSingleDet">Asperiores deleniti quidem quo nobis quae quos alias amet labore nostrum minus consectetur corrupti esse nihil, vel odio ducimus temporibus quaerat qui!</p>
-                            </li>
-                            <li>
-                                <p class="cvSingleTitle">Quis molestias reprehenderit repellat placeat.</p>
-                                <p class="cvSingleDet">Adipisci tenetur, voluptate architecto omnis obcaecati earum cum laborum dolores vero ipsam tempore officiis voluptatum delectus inventore, corporis repellat quo pariatur accusantium?</p>
-                            </li>
-                        </ul>
-                    </div>
+                            <?php } ?>
+                            <div class="cvSubCont">
+                                <ul>
+
+                                    <?php $subSectionId = $subSection['0']['subId']; ?>
+                                    <?php if (isset($this->params['user_forms'][$subSectionId])) { ?>
+                                        <?php $sub_sections_count = count($this->params['user_forms'][$subSectionId]); ?>
+                                    <?php } else { ?>
+                                        <?php $sub_sections_count = 1 ?>
+                                    <?php } ?>
+                                    <?php $i = 0; ?>
+                                    <?php for ($u = 0; $u < $sub_sections_count; $u++) { ?>
+                                        <li>
+                                            <?php foreach ($subSection as $key => $form) { ?>
+                                                <?php if ($key === 0) { ?>
+                                                    <?php continue; ?>
+                                                <?php } ?>
+                                                <?php $value = ''; ?>
+                                                <?php if (isset($this->params['user_forms'][$subSectionId][$u][$form['formId']])) { ?>
+                                                    <?php $value = $this->params['user_forms'][$subSectionId][$u][$form['formId']] ?>
+                                                <?php } ?>
+                                                <?php if ($form['formType'] === 'input') { ?>
+                                                    <?php $type = $form['formNumeric'] == 0 ? 'text' : 'number' ?>
+                                                                                                                                                                    <!--<input class='textInput formControl' form-id="<?= $form['formId'] ?>" index="<?= $i ?>" value="<?= $value ?>" name="Users[custom_fields][<?= $form['formId'] ?>][]" placeholder="<?= $form['formPlaceholder'] ?>" type="<?= $type ?>" />-->
+                                                    <p class="<?= $key === 1 ? 'cvSingleTitle' : 'cvSingleDet' ?>" ><?= $value ?></p>
+                                                <?php } ?>
+                                                <?php if ($form['formType'] === 'textarea') { ?>
+                                                    <!--<textarea class='inputTextarea formControl' form-id="<?= $form['formId'] ?>" index="<?= $i ?>" name="Users[custom_fields][<?= $form['formId'] ?>][]" placeholder="<?= $form['formPlaceholder'] ?>"><?= $value ?></textarea>-->
+                                                    <p class="<?= $key === 1 ? 'cvSingleTitle' : 'cvSingleDet' ?>" ><?= $value ?></p>
+                                                <?php } ?>
+                                                <?php if ($form['formType'] === 'select') { ?>
+                                                    <?php $options = str_replace('-,-', ',', $form['formOptions']); ?>
+                                                    <div class="labelValue">
+                                                        <label><?= $form['formLabel'] ?></label>
+                                                        <span><?= $value ?></span>
+                                                    </div>
+                                                <?php } ?>
+                                                <?php if ($form['formType'] === 'checkbox') { ?>
+                                                    <?php $options = explode('-,-', $form['formOptions']); ?>
+                                                    <?php $values = ''; ?>
+                                                    <?php foreach ($options as $option) { ?>
+                                                        <?php if (isset($this->params['user_forms'][$subSectionId][$u][$form['formId']])) { ?>
+                                                            <?php if (is_array($this->params['user_forms'][$subSectionId][$u][$form['formId']])) { ?>
+                                                                <?php if (in_array($option, $this->params['user_forms'][$subSectionId][$u][$form['formId']])) { ?>
+                                                                    <?php $values .= $option . ' '; ?>
+                                                                <?php } ?>
+                                                            <?php } else { ?>
+                                                                <?php if ($option === $this->params['user_forms'][$subSectionId][$u][$form['formId']]) { ?>
+                                                                    <?php $values = $option; ?>
+                                                                <?php } ?>   
+                                                            <?php } ?>
+                                                        <?php } ?>
+
+                                                    <?php } ?>
+                                                    <div class="labelValue">
+                                                        <label><?= $form['formLabel'] ?></label>
+                                                        <span><?= $values ?></span>
+                                                    </div>
+                                                <?php } ?>
+                                                <?php if ($form['formType'] === 'radio') { ?>
+                                                    <div class="labelValue">
+                                                        <label><?= $form['formLabel'] ?></label>
+                                                        <span><?= $value ?></span>
+                                                    </div>
+                                                <?php } ?>
+                                                <p class="message"></p> 
+                                                <?php $value = ''; ?>
+                                            <?php } ?>
+                                        </li>
+
+                                        <?php $i++; ?>
+                                    <?php } ?>
+                                </ul>
+
+                            </div>
+
+                        </div>
+                    <?php } ?>
                 </div>
-                <div class="cvSub">
-                    <div class="cvSubLabel">
-                        <h5>School</h5>
-                    </div>
-                    <div class="cvSubCont">
-                        <ul>
-                            <li>
-                                <p class="cvSingleTitle">Lorem ipsum dolor sit amet</p>
-                                <p class="cvSingleDet">Fugiat molestias odit sapiente ipsam inventore maiores voluptates sed doloribus beatae explicabo, nisi minima quae iusto, facere velit sunt commodi.</p>
-                                <div class="labelValue">
-                                    <label>Some Label</label>
-                                    <span>Blah blha value</span>
-                                </div>
-                                <div class="labelValue">
-                                    <label>Another label</label>
-                                    <span>Blah blha value</span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="cvTimeline">
-                <h4>military service</h4>
-                <div class="cvSub">
-                    <div class="cvSubCont">
-                        <ul>
-                            <li>
-                                <p class="cvSingleTitle">Lorem ipsum dolor sit amet</p>
-                                <p class="cvSingleDet">Fugiat molestias odit sapiente ipsam inventore maiores voluptates sed doloribus beatae explicabo, nisi minima quae iusto, facere velit sunt commodi.</p>
-                            </li>
-                            <li>
-                                <p class="cvSingleTitle">At eos, delectus cum deleniti non.</p>
-                                <p class="cvSingleDet">Asperiores deleniti quidem quo nobis quae quos alias amet labore nostrum minus consectetur corrupti esse nihil, vel odio ducimus temporibus quaerat qui!</p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <?php } ?>
+
+
         </div>
     </div>
 </div>
