18e72a50761bf27575a12e2354274e16300c6524
Hrayr <lincerta@gmail.com>
2015-09-04 12:52:52 +0400

    st

--- views/users/contact-us.php
+++ views/users/contact-us.php
@@ -1,7 +1,49 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: artur
- * Date: 9/4/15
- * Time: 11:41 AM
- */
+<div class="titleWithBg aboutTitle">
+    <h3>About Us</h3>
+</div>
+<div class="container mainContainer">
+    <div class="aboutCont">
+        <div class="imgTxt">
+            <div class="imgTxtT">
+                <h4>Veritatis facere eum quo.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quas, ratione, nisi, maiores sapiente laudantium et vitae quae iste tempora dolores voluptas quo, quod distinctio fugit fugiat deleniti. Dolores, et!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate nostrum necessitatibus blanditiis, culpa, vitae eveniet quos odio, consectetur atque dolor est a fugit perferendis vero voluptate aliquid alias. Alias, id! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illum a minus, expedita dolorum. Accusantium magni velit aspernatur dolorem quia voluptas doloremque quas consequatur a necessitatibus eos, nisi similique! Cum.</p>
+            </div>
+            <div class="imgTxtI">
+                <img src="images/about-img-1.jpg" alt="">
+            </div>
+        </div>
+        <div class="imgTxt">
+            <div class="imgTxtT">
+                <h4>Suscipit facilis maiores iure voluptates.</h4>
+                <p>Culpa, vitae eveniet quos odio, consectetur atque dolor est a fugit perferendis vero voluptate aliquid alias. Alias, id! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illum a minus, expedita dolorum. Accusantium magni velit aspernatur dolorem quia voluptas doloremque quas consequatur a necessitatibus eos, nisi similique! Cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum enim placeat nemo sapiente id molestias quo corrupti non ex velit, ipsam odit dolorum eligendi rem facere deleniti? Doloribus, mollitia, ullam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus pariatur fugit, doloribus ab officiis illum ut dolores, magnam quo totam rerum sapiente esse voluptate. Porro, unde pariatur. Explicabo, facilis tempore.</p>
+            </div>
+            <div class="imgTxtI">
+                <img src="images/about-img-2.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="contactWrapp">
+        <div class="pageTitle">
+            <h3>Contact us</h3>
+        </div>
+        <div class="formRow">
+            <label>Your Name</label>
+            <input type="text" class="formControl">
+        </div>
+        <div class="formRow">
+            <label>Your Email Address</label>
+            <input type="text" class="formControl">
+        </div>
+        <div class="formRow">
+            <label>Subject</label>
+            <input type="text" class="formControl">
+        </div>
+        <div class="formRow">
+            <label>Your Message</label>
+            <textarea class="formControl"></textarea>
+        </div>
+        <div class="submitSect">
+            <button type="submit" class="btn defBtn"><i class="icon-paper-plane"></i>Send</button>
+        </div>
+    </div>
+</div>
