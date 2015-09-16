b20fc6d7097c065663071468ee3e321d98314597
aram <aram@st-dev.com>
2015-09-16 18:38:40 +0400

    resetPassword

--- mail/resetPassword.php
+++ mail/resetPassword.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
  
 $confirmLink = Yii::$app->urlManager->createAbsoluteUrl([$action,'action'=>'reset', 'id' => $user->id, 'key' => $user->password_reset_token]).'#forgpass-popup-2';
 ?>
-<img src="<?= \Yii::getAlias('@webroot') ?>/images/user-1.png">
+<img src="<?= \Yii::getAlias('@webroot') ?>/images/logo.png">
 
 Dear <?= Html::encode($user->first_name.' '.$user->last_name) ?>,<br>
 For resetting your password please follow this <?= Html::a(Html::encode($confirmLink), $confirmLink) ?> <br>
