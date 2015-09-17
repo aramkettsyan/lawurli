eba8376ed4025791e812b954a2fcc3fd5ad2b1f6
aram <aram@st-dev.com>
2015-09-17 18:41:26 +0400

    news pagination

--- views/news/load-news.php
+++ views/news/load-news.php
@@ -1,26 +1,42 @@
-<?php if(!empty($resources)){ ?>    
-<div style="position: relative">   
+<?php if (!empty($resources)) { ?>    
+    <div style="position: relative">   
         <ul id="newsContent" class="news_cont">
             <?php foreach ($resources as $xmlObject) { ?>
                 <li>
                     <p style="font-weight:bold"><a href="<?php echo $xmlObject['link']; ?>" target="_blank"><?php echo $xmlObject['title']; ?></a></p>
-                    <time><?php echo $xmlObject['pubDate']; ?></time>
+                    <?php if (!empty($xmlObject['pubDate'])) { ?>
+                        <time><?php echo $xmlObject['pubDate']; ?></time>
+                    <?php } ?>
                 </li>
             <?php } ?>
         </ul>
+        <div class="pagin">
+            <ul class="pagination">
+                <?php echo $current_page != 0 ? '<li id="previous_page" class="prev"><a style="cursor:pointer">«</a></li>' : '<li class="prev disabled"><span>«</span></li>' ?>
+                <?php $count = 0; ?>
+                <?php $checkPos = 5 - ($pagesCount - $current_page) ?>
+                <?php for ($i = 0; $i < $pagesCount; $i++) { ?>
+                    <?php if ($current_page == $i) { ?>
+                        <?php $count++; ?>
+                        <li class="pagination_item active" id="<?= $i ?>"><a><?= $i + 1 ?></a></li>&nbsp;
+                    <?php } else if ($i < $current_page && abs($i - $current_page) < 6 || $checkPos > 0) { ?>
+                        <?php if ($checkPos > 0) { ?>
+                            <?php $checkPos--; ?>
+                        <?php } ?>
+                        <?php $count++; ?>
+                        <li class="pagination_item" style="cursor: pointer" id="<?= $i ?>"><a><?= $i + 1 ?></a></li>&nbsp;
+                    <?php } ?>
+                    <?php if ($i > $current_page && $count < 10) { ?>
+                        <?php $count++; ?>
+                        <li class="pagination_item" style="cursor: pointer" id="<?= $i ?>"><a><?= $i + 1 ?></a></li>&nbsp;
+                            <?php } ?>
+                        <?php } ?>
+                        <?php echo ($current_page != ($pagesCount - 1)) ? '<li id="next_page" class="next"><a style="cursor:pointer">»</a></li>' : '<li class="next disabled"><span>»</span></li>' ?>
+            </ul>
+        </div>
     </div>   
 
-    <div>
-        <?php echo $current_page != 0 ? '<span id="previous_page"><</span>' : '' ?>
-        <?php for ($i = 0; $i < $pagesCount; $i++) { ?>
-            <?php if ($current_page == $i) { ?>
-                <span class="pagination_item" style="color:red" id="<?= $i ?>"><?= $i + 1 ?></span>&nbsp;
-            <?php } else if (abs($i - $current_page) < 5) { ?>
-                <span class="pagination_item" style="cursor: pointer" id="<?= $i ?>"><?= $i + 1 ?></span>&nbsp;
-            <?php } ?>
-        <?php } ?>
-        <?php echo ($current_page != ($pagesCount - 1)) ? '<span id="next_page">></span>' : '' ?>
-    </div>
+
 
     <script type="text/javascript">
         $(document).ready(function () {
@@ -47,7 +63,7 @@
             });
         });
     </script>
-<?php }else{ ?>
+<?php } else { ?>
     <div>
         There are no news...
     </div>
