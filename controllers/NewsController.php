b18a52ada971835128dc8ef3a5c30876ec40ec08
aram <aram@st-dev.com>
2015-09-17 16:21:50 +0400

    news

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -125,7 +125,19 @@ class NewsController extends \yii\web\Controller {
             $content = $this->get_data($urls);
             foreach ($content as $c) {
                 try {
-                    $xmlObject = new \SimpleXmlElement($c);
+                    libxml_use_internal_errors(true);
+                    $xmlObject = simplexml_load_string($c);
+                    $xml = explode("\n", $c);
+
+                    if (!$xmlObject) {
+                        $errors = libxml_get_errors();
+
+                        foreach ($errors as $error) {
+//                            echo display_xml_error($error, $xml);
+                        }
+
+                        libxml_clear_errors();
+                    }
                 } catch (Exception $ex) {
                     
                 }
@@ -138,7 +150,7 @@ class NewsController extends \yii\web\Controller {
             $k = 0;
             foreach ($resources as $resource) {
                 $j = 0;
-                if (is_object($resource->channel->item)) {
+                if (is_object($resource) && is_object($resource->channel) && is_object($resource->channel->item)) {
 
                     foreach ($resource->channel->item as $item) {
                         if ($j === 15) {
@@ -165,7 +177,7 @@ class NewsController extends \yii\web\Controller {
                         $k++;
                         $j++;
                     }
-                } else if (is_object($resource->entry)) {
+                } else if (is_object($resource) && is_object($resource->entry)) {
                     foreach ($resource->entry as $item) {
                         if ($j === 15) {
                             break;
@@ -197,10 +209,10 @@ class NewsController extends \yii\web\Controller {
             $model->deleteAll();
             \Yii::$app->db->createCommand('ALTER TABLE news AUTO_INCREMENT = 1')->execute();
             \Yii::$app->db->createCommand()->batchInsert('news', ['news_title', 'news_pub_date', 'news_url', 'created', 'modified'], $rows)->execute();
-        }else{
+        } else {
             throw new \yii\web\NotFoundHttpException();
         }
-        
+
         return;
     }
 
