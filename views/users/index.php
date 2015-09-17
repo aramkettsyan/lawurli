b5087ac4da7ad8260012a886f497a25c6212ff54
aram <aram@st-dev.com>
2015-09-17 10:31:12 +0400

    i

--- views/users/index.php
+++ views/users/index.php
@@ -10,7 +10,7 @@ use yii\widgets\ActiveForm;
 <?php
 $this->title = 'Home'
 ?>
-<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>
+<?php echo "http://" . $_SERVER['SERVER_NAME']; ?>
 <div class="homeTop <?= !Yii::$app->user->isGuest ? 'homeTopUser' : '' ?> clearAfter">
     <div class="flexslider">
             <ul class="slides">
